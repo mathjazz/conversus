1eec9ba41715c03e6719b01a4a2fa8a9cbe6b01a
Matjaž Horvat <matjaz.horvat@gmail.com>
2011-10-21 18:52:19 +0200

    First push

---
+++ publish/js/script.js
@@ -0,0 +1,28 @@
+/* Author: Mozeg, d.o.o.
+
+*/
+
+$(function() {
+	// Navigation
+	$('nav a').click(function() {
+		$('nav a').removeClass('active');
+		$(this).addClass('active');
+	});
+	
+	// Scroll Top
+	$('.top').click(function() {
+		$('nav a').removeClass('active');
+		$('h1 a').addClass('active');
+	});
+	
+	// Smooth Scroll
+	$('a:not(".email")').smoothScroll();
+	
+	// Team
+	$('#team li').hover(function() {
+		$('#team img').removeClass("active");
+		var img = $(this).find('img').addClass("active");
+		$('#sodelavke .name').html(img.attr('alt'));
+		$('#sodelavke .role').html(img.data('role'));
+	}, function() {});
+});
